35b7c70f3bd193c20fccad5ce4a660d1a8b07b06
TED Vortex <ted.vortex@gmail.com>
2022-09-16 23:54:12 +0200

    build: add tailwind and env

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./layouts/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+      addVariant("child-active", "& > *:active");
+      addVariant("child-focus", "& > *:focus");
+    }
+  ]
+};

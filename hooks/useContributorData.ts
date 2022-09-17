9da5e116296848175c7b74b0384cc2dab27fdcc2
takanome_dev <takanomedev221@gmail.com>
2022-09-17 21:19:01 +0000

    refactor: improve redirect not found page and route (#8)
    closes #3

--- hooks/useContributorData.ts
+++ hooks/useContributorData.ts
@@ -37,7 +37,7 @@ const useContributorData = () => {
     return {
       type: "result",
       data: data?.data ?? [],
-      meta: data?.meta ?? { itemCount: 0 },
+      meta: data?.meta ?? { itemCount: 0 }
       // commenting for now to appease build
       // mutate
     };

24c17a2c1d0cbdce7e6cbbeacd34656ddc68bf33
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2023-12-28 23:13:11 -0800

    data is fetched and structured

--- pages/[owner]/[repo]/prCounts.ts
+++ pages/[owner]/[repo]/prCounts.ts
@@ -1,4 +1,14 @@
 
+const prCounts = async (prData) => {
+  const meta = await count(prData);
+  const prsPerDay = await prPerDay(prData);
+
+  return {
+    meta,
+    prsPerDay
+  };
+};
+
 const count = (prData): { mergedCount: number; closedCount: number; totalCount: number } => {
   let mergedCount = 0;
   let closedCount = 0;
@@ -26,4 +36,24 @@ const count = (prData): { mergedCount: number; closedCount: number; totalCount: 
   };
 };
 
-export default count;
+const prPerDay = (prData) => {
+  // Create an object to store merged PRs per day
+  const mergedPRsPerDay = {};
+
+  prData.data.forEach((item) => {
+    const mergedDate = new Date(item.pr_merged_at).toLocaleDateString(); // Extracting the date
+
+    if (item.pr_is_merged === true && item.pr_merged_at !== "0001-01-01T00:00:00.000Z") {
+      if (!mergedPRsPerDay[mergedDate]) {
+        mergedPRsPerDay[mergedDate] = 0; // Initialize count for the day if it doesn't exist
+      }
+      mergedPRsPerDay[mergedDate]++; // Increment merged PR count for that day
+    }
+  });
+
+  console.log("Merged PRs per day:", mergedPRsPerDay);
+
+  return mergedPRsPerDay;
+}
+
+export default prCounts;

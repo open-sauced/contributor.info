f0ff43605005ba91a7ec000f3c49ecef0a16b399
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2024-01-30 21:06:38 -0800

    still fixing the build, merged error

--- pages/[owner]/[repo]/index.tsx
+++ pages/[owner]/[repo]/index.tsx
@@ -73,12 +73,12 @@ const StatsCard = ({ type, status, count, prevMonthCount }: StatsCardProps) => {
 const DataLabel = ({ label, type }: { label: string; type: "merged" | "closed" | "open" }) => {
   const getColorByType = (type: string) => {
     switch (type) {
-    case "merged":
-      return "bg-violet-400";
-    case "closed":
-      return "bg-red-500";
-    case "open":
-      return "bg-green-500";
+      case "merged":
+        return "bg-violet-400";
+      case "closed":
+        return "bg-red-500";
+      case "open":
+        return "bg-green-500";
     }
   };
 
@@ -145,7 +145,7 @@ const OwnerRepo = ({
             />
             <StatsCard
               type="pr"
-              status="merged"
+              status="closed"
               count={closedPrs.filter((pr) => pr.pr_is_merged === true).length}
               prevMonthCount={prevMonthClosedPrs ? prevMonthClosedPrsMeta.itemCount : undefined}
             />
